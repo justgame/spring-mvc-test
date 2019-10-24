00744923c667d7383366cd6db57d3e7d0fd5b5d9
xiecongle <289243011@qq.com>
2019-10-24 23:36:41 +0800

    添加redis

--- src/main/java/com/github/justgame/springmvctest/SpringMvcTestApplication.java
+++ src/main/java/com/github/justgame/springmvctest/SpringMvcTestApplication.java
@@ -1,26 +1,43 @@
 package com.github.justgame.springmvctest;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableCaching
 @EnableAsync
 @EnableScheduling
 public class SpringMvcTestApplication {
+    public static final String CACHE_NAME = "caffeine";
 
+    @Value("${spring.cache.caffeine.spec:recordStats,maximumSize=300000,expireAfterWrite=10s}")
+    private String caffeineSpec;
     public static void main(String[] args) {
         SpringApplication.run(SpringMvcTestApplication.class, args);
     }
 
+    @Bean("caffeineCacheManager")
+    public CaffeineCacheManager getCaffeineCacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCacheNames(Collections.singleton(CACHE_NAME));
+        cacheManager.setCacheSpecification(caffeineSpec);
+        cacheManager.setAllowNullValues(true);
+        return cacheManager;
+    }
+
     @Bean
-    public Cache cache(CacheManager cacheManager) {
-        return cacheManager.getCache("caffeine");
+    public Cache cache(@Qualifier("caffeineCacheManager") CacheManager cacheManager) {
+        return cacheManager.getCache(CACHE_NAME);
     }
 }
---
+++ src/test/java/com/github/justgame/springmvctest/redis/RedisTest.java
@@ -0,0 +1,40 @@
+package com.github.justgame.springmvctest.redis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author XieCl
+ * @date 2019/10/24 23:21
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+    @Autowired
+    private StringRedisTemplate redis;
+
+    @Test
+    public void testScan() {
+        Set<String> keys = new HashSet<>();
+        String matchPattern = "exam:*";
+        redis.execute((RedisConnection connection) -> {
+            Cursor<byte[]> cursor = connection.scan(ScanOptions.scanOptions().match(matchPattern).build());
+            while (cursor.hasNext()) {
+                keys.add(new String(cursor.next()));
+            }
+            return null;
+        });
+        System.out.println(keys.size());
+        // System.out.println(keys);
+    }
+}
